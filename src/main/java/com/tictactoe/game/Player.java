07459ed4577979b5f92d2637c05413fadb6621bc
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-04-15 06:05:10 +0200

    feat: Implemented Player class for Tic Tac Toe game.

--- src/main/java/com/tictactoe/game/Player.java
+++ src/main/java/com/tictactoe/game/Player.java
@@ -1,5 +1,28 @@
 package com.tictactoe.game;
 
 public class Player {
-    
+    private final String name;
+    private final char symbol;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    public boolean makeMove(Board board, int position) {
+        return board.updateBoard(position, symbol);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
