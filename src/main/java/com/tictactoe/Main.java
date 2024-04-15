a1e2b9653cfb5a78cb5dd2a10f71e5e68c52b33a
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-04-15 06:33:48 +0200

    test: Verified the functionality of the game in Main class.

--- src/main/java/com/tictactoe/Main.java
+++ src/main/java/com/tictactoe/Main.java
@@ -1,5 +1,11 @@
 package com.tictactoe;
 
+import com.tictactoe.game.GameManager;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager(3, 2);
+        gameManager.startGame();
+    }
 }
