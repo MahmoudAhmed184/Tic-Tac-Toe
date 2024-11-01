4f2bb570845b57c3fde808c831be4c3266917acb
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-11-01 13:55:27 +0200

    feat: add Main class as entry point for Tic Tac Toe game
    - Created the Main class to serve as the entry point for running the Tic Tac Toe game.
    - Instantiated the Game class with a 3x3 board and two players to test the gameplay functionality.

---
+++ src/main/java/com/mahmoud_ahmed/Main.java
@@ -0,0 +1,11 @@
+package com.mahmoud_ahmed;
+
+import com.mahmoud_ahmed.game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game(3, 2);
+        game.startGame();
+    }
+}
